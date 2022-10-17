b6266cf4527225e286118a83a8a249253fb1f1d3
SMinhChau <minhchaunguyenmtp@gmail.com>
2022-10-17 08:40:56 +0700

    fix dependencies RestTemplate

--- demojwtResTemplate/src/main/java/com/example/demojwt/model/Product.java
+++ demojwtResTemplate/src/main/java/com/example/demojwt/model/Product.java
@@ -3,14 +3,12 @@ package com.example.demojwt.model;
 
 import lombok.Data;
 
-import javax.persistence.*;
 
-@Entity
+
 @Data
-@Table(name = "products")
+
 public class Product {
-    @Id
-    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+
     private Long productId;
     private String name;
     private Double price;
